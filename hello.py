31104ee2387157c6e16bf839ed74591141f05a48
trjim2k7 <trjim2k5@gmail.com>
2021-04-03 10:33:54 +0100

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,6 @@
+print("Hello World")
+
+print("What is your name?")
+myName = input()
+
+print("Hello {}!".format(myName))
---
+++ test-module.py
@@ -0,0 +1,29 @@
+import sys
+print(sys.executable)
+print(sys.version)
+
+
+class Employee:
+    """A sample Employee class"""
+
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+
+    def test_method(self):
+        pass
+
+    @property
+    def email(self):
+        return '{}.{}@email.com'.format(self.first, self.last)
+
+    @property
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+
+emp_1 = Employee('John', 'Smith')
+
+print(emp_1.first)
+print(emp_1.email)
+print(emp_1.fullname)
